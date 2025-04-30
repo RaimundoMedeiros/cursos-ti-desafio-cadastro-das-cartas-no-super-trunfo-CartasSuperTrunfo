29b25b8982e967c473ca48ddaef89b871913366f
Raimundo Medeiros <ychainstyle@gmail.com>
2025-04-29 21:00:11 -0300

    resolução nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,12 +8,13 @@ int main()
     char estado1, estado2;
     char codigo1[4], codigo2[4];
     char cidade1[30], cidade2[30];
-    int populacao1, populacao2;
+    unsigned long int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontosTuristicos1, pontosTuristicos2;
     float densidadePopulacional1, densidadePopulacional2;
     float pibPerCapita1, pibPerCapita2;
+    float superPoder1, superPoder2;
 
     // Entrada de Dados carta 1
     printf("Digite o estado da carta 1 (A-H): ");
@@ -23,7 +24,7 @@ int main()
     printf("Digite o nome da cidade da carta 1: ");
     scanf(" %[^\n]%*c", cidade1);
     printf("Digite a população da cidade da carta 1: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
     printf("Digite a área da cidade da carta 1 (em km²): ");
     scanf("%f", &area1);
     printf("Digite o PIB da cidade da carta 1 (em bilhões de reais): ");
@@ -39,7 +40,7 @@ int main()
     printf("Digite o nome da cidade da carta 2: ");
     scanf(" %[^\n]%*c", cidade2);
     printf("Digite a população da cidade da carta 2: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
     printf("Digite a área da cidade da carta 2 (em km²): ");
     scanf("%f", &area2);
     printf("Digite o PIB da cidade da carta 2 (em bilhões de reais): ");
@@ -49,36 +50,40 @@ int main()
 
     // Cálculos da densidade e PIB per capita
     densidadePopulacional1 = (float)populacao1 / area1;
+
     densidadePopulacional2 = (float)populacao2 / area2;
 
     // Multiplica o PIB por 1 bilhão para converter para reais
     pibPerCapita1 = (pib1 * BILHAO) / (float)populacao1;
+
     pibPerCapita2 = (pib2 * BILHAO) / (float)populacao2;
 
-    // Saída de Dados carta 1
-    printf("\nCarta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
-    printf("\n");
+    // Cálculo do Super Poder
+    superPoder1 = (float)populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1 / densidadePopulacional1);
 
-    // Saída de Dados carta 2
-    printf("\nCarta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+    superPoder2 = (float)populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1 / densidadePopulacional2);
+
+    // Comparação dos atributos
+    int vPopulacao = populacao1 > populacao2;
+    int vArea = area1 > area2;
+    int vPib = pib1 > pib2;
+    int vPontosTuristicos = pontosTuristicos1 > pontosTuristicos2;
+    int vDensidadePopulacional = densidadePopulacional1 < densidadePopulacional2;
+    int vPibPerCapita = pibPerCapita1 > pibPerCapita2;
+    int vSuperPoder = superPoder1 > superPoder2;
+
+    // Array para exibir vencedor
+    const char *resultado[] = {"Carta 2 venceu (0)", "Carta 1 venceu (1)"};
+
+    // Saída dos resultados:
+    printf("\nComparação de Cartas:\n");
+    printf("População: %s\n", resultado[vPopulacao]);
+    printf("Área: %s\n", resultado[vArea]);
+    printf("PIB: %s\n", resultado[vPib]);
+    printf("Pontos Turísticos: %s\n", resultado[vPontosTuristicos]);
+    printf("Densidade Populacional: %s\n", resultado[vDensidadePopulacional]);
+    printf("PIB per Capita: %s\n", resultado[vPibPerCapita]);
+    printf("Super Poder: %s\n", resultado[vSuperPoder]);
     printf("\n");
 
     return 0;
